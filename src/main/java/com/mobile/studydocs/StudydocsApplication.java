145bd9809f7d7318a95c0beda9d08c7c65d5e831
DuyDZ <22130057@st.hcmuaf.edu.vn>
2025-06-02 01:45:24 +0700

    feature: implement search by title

--- src/main/java/com/mobile/studydocs/StudydocsApplication.java
+++ src/main/java/com/mobile/studydocs/StudydocsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StudydocsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudydocsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StudydocsApplication.class, args);
+    }
 
 }
--- src/main/java/com/mobile/studydocs/model/entity/Document.java
+++ src/main/java/com/mobile/studydocs/model/entity/Document.java
@@ -1,4 +1,23 @@
 package com.mobile.studydocs.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Document {
+    private String userId;
+    private String title;
+    private String description;
+    private String fileUrl;
+    private String subject;
+    private String university;
+    private boolean isDelete;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+    private Timestamp deletedAt;
 }
