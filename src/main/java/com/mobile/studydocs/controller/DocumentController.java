d2d3f0f8c979018e78469aee425f2b0d1aaad6de
LYTDUNG <22130054@st.hcmuaf.edu.vn>
2025-06-06 22:04:39 +0700

    feat: Thêm AuthService để xác thực người dùng,Thêm bucketName từ cấu hình qua @Value trong DocumentController.

--- src/main/java/com/mobile/studydocs/controller/DocumentController.java
+++ src/main/java/com/mobile/studydocs/controller/DocumentController.java
@@ -1,18 +1,19 @@
 package com.mobile.studydocs.controller;
 
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
 import com.mobile.studydocs.model.dto.SearchDTO;
 import com.mobile.studydocs.response.BaseResponse;
 import com.mobile.studydocs.service.DocumentService;
-import com.mobile.studydocs.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
+import com.mobile.studydocs.service.AuthService;
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.concurrent.ExecutionException;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 @RequestMapping("/controller")
@@ -20,6 +21,7 @@ public class DocumentController {
     private final DocumentService documentService;
     private final Storage storage; // Thêm Storage để xử lý download
     private final AuthService authService; // Thêm AuthService để kiểm tra người dùng đăng nhập
+    private final String bucketName;
 
     public DocumentController(DocumentService documentService) {
         this.documentService = documentService;
@@ -36,10 +38,11 @@ public class DocumentController {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(new BaseResponse(HttpStatus.OK.value(), "Lấy danh sách thành công", searchDTO));
     }
-    public DocumentController(DocumentService documentService, Storage storage, AuthService authService) {
+    public DocumentController(DocumentService documentService, Storage storage, AuthService authService, @Value("${firebase.bucket-name}") String bucketName) {
         this.documentService = documentService;
         this.storage = storage;
         this.authService = authService;
+        this.bucketName = bucketName;
     }
     /**
      * Xem chi tiết tài liệu theo ID
@@ -132,5 +135,4 @@ public class DocumentController {
                     .body(new BaseResponse(HttpStatus.NOT_FOUND.value(), "Tài liệu không tồn tại", false));
         }
     }
-}
 }
