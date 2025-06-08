29bc4e4509a5c03e5a53875944398b34c59eab37
HuynhMinhHien-22130080 <22130080@st.hcmuaf.edu.vn>
2025-06-08 12:55:36 +0700

    Feat: Consumer cho DocumentProuducer (#8)
    * refactor(eda): decouple event producer
    
    * fix(firebase): ensure FirebaseApp is initialized before using Storage and Firestore
    
    * feat(follow): add follow/unfollow logic, models, services, and Firestore integration
    
    * refactor(follow): triển khai chiến lược sharding trong UserDao để tối ưu lưu trữ follower/following
    
    * refactor(response): Thêm class BaseResponse hỗ trợ phản hồi cho client theo định dạng: {status, message, data}
    
    * feat(exception): thêm hệ thống ngoại lệ tùy chỉnh và bộ xử lý ngoại lệ toàn cục
    
    * feat(user-controller): thay đổi request mapping-> post/get
    
    * feat(consumer-notificationn): Thêm consumer cho notify
    
    ---------
    
    Co-authored-by: hien <22130080@st.hcmuaf.edu.vn>

---
+++ src/main/java/com/mobile/studydocs/model/entity/Notifications.java
@@ -0,0 +1,26 @@
+package com.mobile.studydocs.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Notifications {
+    private String senderId;
+    private String documentId;
+    private String type;
+    private String title;
+    private String message;
+    @Builder.Default
+    private Timestamp createAt = new Timestamp(System.currentTimeMillis());
+    @Builder.Default
+    private boolean isRead = false;
+
+
+}
